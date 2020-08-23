d2813d31bd4f9d4d93aea1e9490d4db4353b9a17
Tanya <REINACREATES1@GMAIL.com>
2020-08-23 16:19:13 -0400

    first commit

--- code.js
+++ code.js
@@ -2,16 +2,21 @@ window.onload = function () { // Run this once the page has loaded.
     // search
     document.querySelector("#searchButton").addEventListener("click", searchGithub);
     function searchGithub() {
-        const url = "https://api.github.com/search/users?q="
+        const url = "https://api.github.com/search/users?q=";
         const searchUserText = document.querySelector("#searchUser").value;
 
         // Obtain a list of users from the Github API that match searchUserText
         //  The final result will contain an array under the key 'items'
         // Pass this array to `renderUserList`
     }
+    window.onload = function () {
+    const BASE_URL = "https://api.github.com/search/users?q=";
+    fetch(BASE_URL + "searchUserText")
+    .then((response) => response.json())
+    .then((result) => renderUserList(result));
 
     function renderUserList(githubUsers) {
-        let html = "";
+            let html = "";
         html += "<ul>";
         for (let i = 0; i < githubUsers.length; i++) {
             let githubUser = githubUsers[i];
