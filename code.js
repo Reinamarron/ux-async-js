54c8dd3b7b1844ac6907f68f56065534e697c11d
Tanya <REINACREATES1@GMAIL.com>
2020-09-22 22:29:34 -0400

    updated html and js

--- code.js
+++ code.js
@@ -2,19 +2,18 @@ window.onload = function () { // Run this once the page has loaded.
     // search
     document.querySelector("#searchButton").addEventListener("click", searchGithub);
     function searchGithub() {
-        const url = "https://api.github.com/search/users?q=";
         const searchUserText = document.querySelector("#searchUser").value;
 
         // Obtain a list of users from the Github API that match searchUserText
         //  The final result will contain an array under the key 'items'
         // Pass this array to `renderUserList`
-    }
-    window.onload = function () {
-    const BASE_URL = "https://api.github.com/search/users?q=";
-    fetch(BASE_URL + "searchUserText")
-    .then((response) => response.json())
-    .then((result) => renderUserList(result));
-
+      
+            const BASE_URL = "https://api.github.com/search/users?q=";
+            fetch(BASE_URL + searchUserText)
+            .then((response) => response.json())
+            .then((result) => renderUserList(result.items));
+            
+        }
     function renderUserList(githubUsers) {
             let html = "";
         html += "<ul>";
@@ -26,7 +25,6 @@ window.onload = function () { // Run this once the page has loaded.
             html += "</li>";
         }
         html += "</ul>";
-
         document.querySelector("#resultsContainer").innerHTML = html;
     }
 }
